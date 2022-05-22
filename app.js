b163af9da98f708ef84974bce0f2fe5f51daaa3c
singh-gyan <82334826+singh-gyan@users.noreply.github.com>
2022-05-22 21:27:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+const express = require('express');
+const spotifywebapi = require('spotify-web-api-node');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+const spotify = new spotifywebapi({
+  redirectUri: process.env.REDIRECT_LINK,
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET_ID,
+});
+let accessToken;
+
+app.post('/login', async (req, res) => {
+  const code = req.body.code;
+
+  await spotify
+    .authorizationCodeGrant(code)
+    .then(data => {
+      accessToken = data.body.access_token;
+
+      res.json({
+        accesstoken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresin: data.body.expires_in,
+      });
+    })
+    .catch(err => {
+      // console.log(err);
+      // res.send(err);
+      res.sendStatus(404);
+    });
+  console.log(accessToken);
+  spotify.setAccessToken(accessToken);
+});
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new spotifywebapi({
+    redirectUri: process.env.REDIRECT_LINK,
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET_ID,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+async function searchArtist(artist) {
+  const res = await spotify.searchArtists(artist, { limit: 1 });
+  // console.table(res.body.artists.items);
+  const albums = await spotify.getArtistAlbums(res.body.artists.items[0].id);
+  console.log(albums.body.items);
+  return albums.body.items;
+}
+app.post('/', (req, res) => {
+  req.body.search.forEach(item => searchArtist(item));
+});
+app.listen(3001, console.log('listening on port 3001'));
